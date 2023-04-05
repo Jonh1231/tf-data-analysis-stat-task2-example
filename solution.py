347c43ac495a870a3e6d55e162a44c9e1578c442
Jonh1231 <129982895+Jonh1231@users.noreply.github.com>
2023-04-05 20:21:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,21 @@
-import pandas as pd
-import numpy as np
-
-from scipy.stats import norm
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+def solution2(p: float, x: np.array) -> tuple: 
+    # Измените код этой функции 
+    # Это будет вашим решением 
+    # Не меняйте название функции и её аргументы 
+    time_92 = 92 
+    alpha = 1 - p 
+    semians = 0 
+    for i in range(len(x)): 
+      semians += x[i] 
+    semians /= len(x) 
+    left = semians + scipy.stats.gamma.ppf((1-alpha)/2, len(x)) 
+    right = semians + scipy.stats.gamma.ppf((1+alpha)/2, len(x)) 
+    left -= 1/2 
+    right -= 1/2 
+    left /= (time_92*time_92) 
+    right /= (time_92*time_92) 
+    left *= 2 
+    right *= 2 
+ 
+    return left, \ 
+           right 
